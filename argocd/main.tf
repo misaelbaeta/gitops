0786018e25f8079861c9640ae54923557c9eba81
Misael Oliveira <misaeloliveira@MacBook-Pro-de-SytDigital.local>
2024-09-16 15:50:33 -0300

    adjusts argocd

--- argocd/main.tf
+++ argocd/main.tf
@@ -1,7 +1,11 @@
+provider "aws" {
+  region = var.AWS_REGION
+}
+
+
 locals {
-  environment  = "staging"
-  cluster_name = "staging-eks"
-  region      = "us-east-1"
+  cluster_name = "sre"
+  region       = "us-east-1"
 }
 
 terraform {
@@ -15,33 +19,35 @@ terraform {
       version = "2.15.0"
     }
   }
+
+  backend "s3" {
+    bucket = "sre-us-east-1"
+    key    = "argocd/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
-data "aws_eks_cluster" "default" {
+data "aws_eks_cluster" "sre" {
   name = local.cluster_name
 }
 
-data "aws_eks_cluster_auth" "default" {
+data "aws_eks_cluster_auth" "sre" {
   name = local.cluster_name
 }
 
-provider "aws" {
-  profile = "dpe-${local.environment}"
-  region  = local.region
-}
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.default.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.default.token
+    host                   = data.aws_eks_cluster.sre.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.sre.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.sre.token
   }
 }
 
-data "kubernetes_service" "argocd_server" {
-  count = var.create_argcd_dns_record && var.create_argocd ? 1 : 0
-  metadata {
-    name      = "argocd-server"
-    namespace = "argocd"
-  }
-}
+#data "kubernetes_service" "argocd_server" {
+#  count = var.create_argcd_dns_record && var.create_argocd ? 1 : 0
+#  metadata {
+#    name      = "argocd-server"
+#    namespace = "argocd"
+#  }
+#}
